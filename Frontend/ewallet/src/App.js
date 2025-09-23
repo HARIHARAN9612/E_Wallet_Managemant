f4e379d9cb9908d6f6588fb35767f08129e66fcf
HARIHARAN9612 <hariharan9612@gmail.com>
2025-09-23 09:21:57 +0530

    AboutSection

--- Frontend/ewallet/src/App.js
+++ Frontend/ewallet/src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Navbar from "./Landing/Nav";
 import Land from "./Landing/Land";
-import Features from "./Landing/Features";
+import Features from "./Landing/Features";  
+import About from "./Landing/About";
 
 
 
 function App() {
   return (
     <div>
-       <Navbar />
+       <Navbar/>
        <Land/>
        <Features/>
-     
+     <About/>
     </div>
   );
 }
---
+++ Frontend/ewallet/src/Landing/About.js
@@ -0,0 +1,15 @@
+
+const About = () => {
+    return (
+        <div className="about-section">
+            {/* Top Heading */}
+            <div className="about-header">      
+                <h2>About Us</h2>
+                <p>
+                Wishpay is a cutting-edge digital wallet and payment platform designed to simplify and enhance your financial transactions. Our mission is to provide a secure, user-friendly, and efficient way for individuals and businesses to manage their money, make payments, and transfer funds seamlessly.
+                </p>
+            </div>     
+            </div>             
+            );
+};
+export default About;
